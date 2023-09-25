da47e841b2f8a6cdb57848d6af0afd7c06f7ae65
Lucas Chagas <107507922+llucaschagass@users.noreply.github.com>
2023-09-25 14:39:31 -0300

    first commit

---
+++ Código/api-santander-bootcamp-2023/src/main/java/me/dio/apisantanderbootcamp2023/ApiSantanderBootcamp2023Application.java
@@ -0,0 +1,13 @@
+package me.dio.apisantanderbootcamp2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSantanderBootcamp2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSantanderBootcamp2023Application.class, args);
+	}
+
+}
---
+++ Código/api-santander-bootcamp-2023/src/test/java/me/dio/apisantanderbootcamp2023/ApiSantanderBootcamp2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.apisantanderbootcamp2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSantanderBootcamp2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
